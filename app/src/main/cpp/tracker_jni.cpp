89aab6b2ab2f44c8cabdee369c648e64d9c45e42
Administrator <admin@example.com>
2025-09-01 19:12:30 +0800

    修复JNI方法缺失问题: 添加updateThresholds和inferRGBAWithTiming方法声明

--- app/src/main/cpp/tracker_jni.cpp
+++ app/src/main/cpp/tracker_jni.cpp
@@ -6,6 +6,8 @@ extern "C" {
     JNIEXPORT jboolean JNICALL Java_com_example_tracker_NativeBridge_init(JNIEnv* env, jclass clazz, jstring jparam, jstring jbin, jint inputSize, jint numClasses);
     JNIEXPORT void JNICALL Java_com_example_tracker_NativeBridge_setImageSize(JNIEnv* env, jclass clazz, jint w, jint h);
     JNIEXPORT jint JNICALL Java_com_example_tracker_NativeBridge_inferRGBA(JNIEnv* env, jclass clazz, jobject buf, jint w, jint h, jbyteArray outFrame);
+    JNIEXPORT jfloat JNICALL Java_com_example_tracker_NativeBridge_inferRGBAWithTiming(JNIEnv* env, jclass clazz, jobject buf, jint w, jint h, jbyteArray outFrame);
+    JNIEXPORT void JNICALL Java_com_example_tracker_NativeBridge_updateThresholds(JNIEnv* env, jclass clazz, jfloat confThresh, jfloat nmsThresh);
     JNIEXPORT void JNICALL Java_com_example_tracker_NativeBridge_reset(JNIEnv* env, jclass clazz);
     JNIEXPORT void JNICALL Java_com_example_tracker_NativeBridge_release(JNIEnv* env, jclass clazz);
     JNIEXPORT jstring JNICALL Java_com_example_tracker_NativeBridge_version(JNIEnv* env, jclass clazz);
